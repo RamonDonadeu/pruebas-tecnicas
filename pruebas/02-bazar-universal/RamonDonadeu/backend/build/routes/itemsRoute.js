212819d0122464b66dc79b7cad896a1c083b5545
RamonDonadeu <ramondonadeucaballero@gmail.com>
2023-10-19 14:45:36 +0200

    Remove from GitIgnore

--- pruebas/02-bazar-universal/RamonDonadeu/backend/build/routes/itemsRoute.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const { Router } = require("express");
-const { getItem, getItemsQuery } = require("../controller/itemsController.js");
-const router = Router();
-router.get("/:id", getItem);
-router.get("/", getItemsQuery);
-module.exports = router;
-//# sourceMappingURL=itemsRoute.js.map
