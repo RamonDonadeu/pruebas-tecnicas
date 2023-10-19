08df362d7114a42d48b9e2cfdf14d2f4bc65f858
RamonDonadeu <ramondonadeucaballero@gmail.com>
2023-10-19 13:00:11 +0200

    make it better

--- pruebas/02-bazar-universal/RamonDonadeu/backend/index.js
+++ pruebas/02-bazar-universal/RamonDonadeu/backend/index.js
@@ -1,8 +1,11 @@
 import express, { json } from "express";
 import itemsRoute from "./routes/itemsRoute.js";
+import functions from "firebase-functions";
+import cors from "cors";
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
+app.use(cors());
 app.use(json());
 
 app.use("/api/item", itemsRoute);
@@ -10,3 +13,5 @@ app.use("/api/item", itemsRoute);
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+exports.api = functions.https.onRequest(app);
